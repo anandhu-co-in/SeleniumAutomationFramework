98ba932defb87544985d395f5d866db5b67582f1
Anandhukrishnan V <mail.anandhu@gmail.com>
2021-02-20 19:13:13 +0530

    impletemented testng xml and parameterization to select browser. need to add IE and firefox and edge exes

--- src/test/java/com/testClasses/BaseClass.java
+++ src/test/java/com/testClasses/BaseClass.java
@@ -5,8 +5,11 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
     public static WebDriver driver;
@@ -18,11 +21,25 @@ public class BaseClass {
     public String username = readconfig.gerConfig("username");
     public String password = readconfig.gerConfig("password");
 
+    @Parameters("browser")
     @BeforeClass
-    public void setup() {
+    public void setup(String browser) {
 
-        System.setProperty("webdriver.chrome.driver", readconfig.gerConfig("chromepath"));
-        driver = new ChromeDriver();
+        if(browser.equals("chrome"))
+        {
+            System.setProperty("webdriver.chrome.driver",readconfig.gerConfig("chromepath"));
+            driver=new ChromeDriver();
+        }
+        else if(browser.equals("firefox"))
+        {
+            System.setProperty("webdriver.gecko.driver",readconfig.gerConfig("iepath"));
+            driver = new FirefoxDriver();
+        }
+        else if(browser.equals("ie"))
+        {
+            System.setProperty("webdriver.ie.driver",readconfig.gerConfig("firefoxpath"));
+            driver = new InternetExplorerDriver();
+        }
 
         logger = Logger.getLogger("LoggerName");
         PropertyConfigurator.configure("log4j.properties");
